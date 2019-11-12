14ab06676be8734b40336d2e960a8b790c958df6
valdirleyoliveira78 <57117420+valdirleyoliveira78@users.noreply.github.com>
2019-11-11 22:21:49 -0300

    Add files via upload

--- CalculoIMC/IMC.js
+++ CalculoIMC/IMC.js
@@ -3,16 +3,44 @@
  * @author Valdirley Oliveira
  */
 
- // Variáveis Globais
- let Peso, Altura, Imc
+// Variáveis Globais
+let Peso, Altura, Imc
 
- function calcular(){
-Altura= Number(frmImc.txtAltura.value.replace(",","."));
-Peso= Number(frmImc.txtPeso.value.replace(",","."));
-Imc = (Peso / ( Altura * Altura))
-frmImc.txtResultado.value=Imc.toFixed(2)  
+function calcular() {
+    Altura = Number(frmImc.txtAltura.value.replace(",", "."));
+    Peso = Number(frmImc.txtPeso.value.replace(",", "."));
+    Imc = (Peso / (Altura * Altura))
+    frmImc.txtResultado.value = Imc.toFixed(2)
 
-}
+    if (imc < 17) {
+            document.getElementById('status').src = 'imc1.png'
+ 
+    else if (imc >= 17 && imc < 18.5) {
+            document.getElementById('status').src = 'imc2.png'
+    
+    else if (imc > 18.5 && imc < 25) {
+            document.getElementById('status').src = 'imc3.png'
+    
+    else if (imc > 25 && imc < 30) {
+            document.getElementById('status').src = 'imc4.png'
+                }
+    else if (imc > 30 && imc < 35) {
+            document.getElementById('status').src = 'imc5.png'
+    
+    else if (imc > 35 && imc < 40) {
+            document.getElementById('status').src = 'imc6.png'
+    
+    else (imc > 40){
+            document.getElementById('status').src = 'imc7.png'
+       }
+      } 
+     }
+    }
+   }
+  }
+
+
+
 
 
 
